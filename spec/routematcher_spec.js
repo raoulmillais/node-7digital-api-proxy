f6b16db7bdbf92448ce6e6e56a30aa8691b58ddf
Raoul Millais <raoul@raoulmillais.com>
2011-10-12 14:01:46 +0100

    Add test for matching valid route with pathprefix

--- spec/routematcher_spec.js
+++ spec/routematcher_spec.js
@@ -5,7 +5,7 @@ describe('when matching', function() {
 
 	var options, matcher;
 
-	it('should return a hash with the resolved route', function() {
+	it('should return a hash with the resolved route (no prefix)', function() {
 		var result;
 
 		options = {
@@ -21,5 +21,23 @@ describe('when matching', function() {
 		expect(result.resourceClassName).toEqual('Artists');
 		expect(result.apiMethodName).toEqual('getDetails');
 	});
+
+	it('should return a hash with the resolved route (prefix)', function() {
+		var result;
+
+		options = {
+			pathprefix: 'api',
+			logger: new winston.Logger({ transports: [] }),
+			schema: require('7digital-api')
+		};
+		matcher = new RouteMatcher(options);
+		result = matcher.match({ pathname: '/api/artist/details' });
+		expect(result).toBeDefined();
+		expect(result.resource).toEqual('artist');
+		expect(result.action).toEqual('details');
+		expect(result.resourceClassName).toEqual('Artists');
+		expect(result.apiMethodName).toEqual('getDetails');
+	});
+
 
 });

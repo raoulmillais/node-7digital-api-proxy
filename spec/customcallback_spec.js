ba6bc444e29f01d3eb73aa4e040ca8e858540055
Raoul Millais <raoul@raoulmillais.com>
2011-11-29 16:08:06 +0000

    Make callback binding test more explicit

--- spec/customcallback_spec.js
+++ spec/customcallback_spec.js
@@ -2,22 +2,23 @@ describe('custom callback', function () {
 
 	it('should bind the context', function() {
 		var callBackDone = false,
+			fakeReq = {
+				url: '/artist/details?artistId=1'
+			},
+			fakeRes = {},
 			proxy = require('../lib/apiproxy')({
 				callback: function(err, data) {
-					expect(this.req).toBeDefined();
-					expect(this.res).toBeDefined();
+					expect(this.req).toBe(fakeReq);
+					expect(this.res).toBe(fakeRes);
 					expect(this.actionSlug).toEqual('details');
 					expect(this.apiCall).toEqual('artist/details');
 					expect(this.resourceSlug).toEqual('artist');
 					callBackDone = true;
 				}
-			}),
-			fakeReq = {
-				url: '/artist/details?artistId=1'
-			};
+			});
 
-		proxy(fakeReq, {});
-		
+		proxy(fakeReq, fakeRes);
+
 		waitsFor(function() {
 			return callBackDone == true;
 		}, "Callback was never called", 5000);
